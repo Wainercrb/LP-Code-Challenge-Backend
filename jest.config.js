3357cfb03e6e251d79fb3feb259f7cd5c31ba360
wainerrodriguez <weiner.rodriguez@costaricasoftwareservices.com>
2024-05-02 15:58:32 -0600

    Added missing test and configuration

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    '^@application/(.*)$': '<rootDir>/src/application/$1',
-    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
-    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}'
